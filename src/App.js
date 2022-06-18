647a50b1714654a73a79e1a3ad5439bd5d142820
Casaemiro <silasmagho@gmail.com>
2022-06-18 15:27:02 +0100

    merged

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import Findfood from "./Findfood";
-=======
 import DonationSection from "./DonationSection";
->>>>>>> 43540ede0c3fa49a1c448dac1d584c0a9ff1947d
 import Footer from "./Footer";
 import Introduction from "./Introduction";
 import Navbar from "./Navbar";
@@ -13,14 +10,11 @@ function App() {
     <div className="App">
      <Navbar />
      <Introduction />
-<<<<<<< HEAD
      <Findfood />
      <Foodlocation />
      <Foodlocation />
      <Foodlocation />
-=======
      <DonationSection />
->>>>>>> 43540ede0c3fa49a1c448dac1d584c0a9ff1947d
      <Footer />
     </div>
   );
